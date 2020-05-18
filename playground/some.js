20fd289b0767e475e3d3ba6f6389304b1397c295
petitlapin86 <skinnerpa86@yahoo.com>
2020-05-18 12:00:34 -0400

    data types

--- playground/some.js
+++ playground/some.js
@@ -3,3 +3,13 @@ console.log('im in another file');
 
 const age = 100;
 console.log(p, age);
+
+
+
+// there are data types
+//string === word or letter
+//number === 2
+//boolean === true or false
+//null === nothing
+//undefined === not yet assigned
+//symbol === 
